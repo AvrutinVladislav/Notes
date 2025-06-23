106caa0ee09184eb09179c2f83000e2b91560484
Avrutin Vlad <avrutin1991@gmail.com>
2025-06-23 17:20:23 +0300

    update google.plist

--- NotesApp/App/AppDelegate.swift
+++ NotesApp/App/AppDelegate.swift
@@ -17,8 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        let db = Firestore.firestore()
-        let ref = Database.database().reference()
         return true
     }
 
